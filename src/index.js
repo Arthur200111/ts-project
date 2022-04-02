e2a36bf2463968185618909c9fd6d8cd6d866a88
Arthur200111 <arthur.huet01@gmail.com>
2022-04-02 11:18:15 +0200

    feat: base project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import test from "./truc"
+import './styles/main.scss'
+import cube from './assets/collision.png'
+
+const cubeImg =document.getElementById('cube')
+cubeImg.src = cube
+
+console.log(test())
---
+++ src/truc.js
@@ -0,0 +1,5 @@
+function test() {
+  return "ceci est un testttt"
+}
+
+export default test
