2446b8029bdbd6a2c7b69670e3dc4909e988cf71
Arthur200111 <arthur.huet01@gmail.com>
2022-04-02 15:25:19 +0200

    feat: cubeImg

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import "./styles/main.scss";
 import changeText from "./functionApi";
+import cubeImg from './assets/collision.png'
 
-const textBtn = <HTMLButtonElement>document.getElementById("textBtn");
+const textBtn : HTMLButtonElement = <HTMLButtonElement>document.getElementById("textBtn");
 
 textBtn.addEventListener("click", changeText);
+
+const htmlImg : HTMLImageElement = <HTMLImageElement>document.getElementById('cube')
+htmlImg.src = cubeImg;
 
 changeText();
